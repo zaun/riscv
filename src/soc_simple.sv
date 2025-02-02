068c35ecaa67288e8fcef1619bbb17d984465e38
Justin Zaun <justin.zaun@gmail.com>
2025-02-02 08:33:42 -1000

    Add the tl_ouput module to the simple soc. Some other cleanups

--- src/soc_simple.sv
+++ src/soc_simple.sv
@@ -15,6 +15,7 @@
 `include "tl_switch.sv"
 `include "tl_memory.sv"
 `include "tl_ul_bios.sv"
+`include "tl_ul_output.sv"
 
 `ifndef XLEN
 `define XLEN 32
@@ -45,7 +46,7 @@ module top
 // ──────────────────────────
 input  wire        CLK;              // System Clock
 input  wire        BTN_S1;           // Button for reset
-output reg  [5:0]  LED;              // LEDs
+output reg  [5:0]  LED;              // 6 LEDs
 input  wire        UART_RX;          // Receive line
 output reg         UART_TX;          // Transmit line
 
@@ -64,10 +65,10 @@ output reg  [3:0]  O_sdram_dqm;      // 32/4
 // Parameters
 // ──────────────────────────
 parameter XLEN          = 32;
-parameter SID_WIDTH     = 8;
+parameter SID_WIDTH     = 2;
 parameter NUM_INPUTS    = 1;
-parameter NUM_OUTPUTS   = 2;
-parameter TRACK_DEPTH   = 16;
+parameter NUM_OUTPUTS   = 3;
+parameter TRACK_DEPTH   = 2;
 parameter CLK_FREQ_MHZ  = 27;
 
 // ──────────────────────────
@@ -153,26 +154,57 @@ assign bios_base_address = 32'h8000_0000;
 assign bios_size         = 32'h0000_00FF;
 
 // A Channel
-wire bios_s_a_valid;
-wire bios_s_a_ready;
-wire [2:0] bios_s_a_opcode;
-wire [2:0] bios_s_a_param;
-wire [2:0] bios_s_a_size;
-wire [SID_WIDTH-1:0] bios_s_a_source;
-wire [XLEN/8-1:0] bios_s_a_mask;
-wire [XLEN-1:0] bios_s_a_address;
-wire [XLEN-1:0] bios_s_a_data;
+wire                   bios_s_a_valid;
+wire                   bios_s_a_ready;
+wire [2:0]             bios_s_a_opcode;
+wire [2:0]             bios_s_a_param;
+wire [2:0]             bios_s_a_size;
+wire [SID_WIDTH-1:0]   bios_s_a_source;
+wire [XLEN/8-1:0]      bios_s_a_mask;
+wire [XLEN-1:0]        bios_s_a_address;
+wire [XLEN-1:0]        bios_s_a_data;
 
 // D Channel
-wire bios_s_d_valid;
-wire bios_s_d_ready;
-wire [2:0] bios_s_d_opcode;
-wire [1:0] bios_s_d_param;
-wire [2:0] bios_s_d_size;
-wire [SID_WIDTH-1:0] bios_s_d_source;
-wire [XLEN-1:0] bios_s_d_data;
-wire bios_s_d_corrupt;
-wire bios_s_d_denied;
+wire                   bios_s_d_valid;
+wire                   bios_s_d_ready;
+wire [2:0]             bios_s_d_opcode;
+wire [1:0]             bios_s_d_param;
+wire [2:0]             bios_s_d_size;
+wire [SID_WIDTH-1:0]   bios_s_d_source;
+wire [XLEN-1:0]        bios_s_d_data;
+wire                   bios_s_d_corrupt;
+wire                   bios_s_d_denied;
+
+// ──────────────────────────
+// Slave - output
+// ──────────────────────────
+
+logic [XLEN-1:0] output_base_address;
+logic [XLEN-1:0] output_size;
+assign output_base_address = 32'h0001_0000;
+assign output_size         = 32'h0000_0000;
+
+// A Channel
+wire                   output_s_a_valid;
+wire                   output_s_a_ready;
+wire [2:0]             output_s_a_opcode;
+wire [2:0]             output_s_a_param;
+wire [2:0]             output_s_a_size;
+wire [SID_WIDTH-1:0]   output_s_a_source;
+wire [XLEN/8-1:0]      output_s_a_mask;
+wire [XLEN-1:0]        output_s_a_address;
+wire [XLEN-1:0]        output_s_a_data;
+
+// D Channel
+wire                   output_s_d_valid;
+wire                   output_s_d_ready;
+wire [2:0]             output_s_d_opcode;
+wire [1:0]             output_s_d_param;
+wire [2:0]             output_s_d_size;
+wire [SID_WIDTH-1:0]   output_s_d_source;
+wire [XLEN-1:0]        output_s_d_data;
+wire                   output_s_d_corrupt;
+wire                   output_s_d_denied;
 
 
 // ──────────────────────────
@@ -217,34 +249,34 @@ tl_switch #(
     // ======================
     // A Channel - Slaves
     // ======================
-    .s_a_valid   ({ bios_s_a_valid    , memory_s_a_valid     }),
-    .s_a_ready   ({ bios_s_a_ready    , memory_s_a_ready     }),
-    .s_a_opcode  ({ bios_s_a_opcode   , memory_s_a_opcode    }),
-    .s_a_param   ({ bios_s_a_param    , memory_s_a_param     }),
-    .s_a_size    ({ bios_s_a_size     , memory_s_a_size      }),
-    .s_a_source  ({ bios_s_a_source   , memory_s_a_source    }),
-    .s_a_mask    ({ bios_s_a_mask     , memory_s_a_mask      }),
-    .s_a_address ({ bios_s_a_address  , memory_s_a_address   }),
-    .s_a_data    ({ bios_s_a_data     , memory_s_a_data      }),
+    .s_a_valid   ({ bios_s_a_valid    , memory_s_a_valid   , output_s_a_valid     }),
+    .s_a_ready   ({ bios_s_a_ready    , memory_s_a_ready   , output_s_a_ready     }),
+    .s_a_opcode  ({ bios_s_a_opcode   , memory_s_a_opcode  , output_s_a_opcode    }),
+    .s_a_param   ({ bios_s_a_param    , memory_s_a_param   , output_s_a_param     }),
+    .s_a_size    ({ bios_s_a_size     , memory_s_a_size    , output_s_a_size      }),
+    .s_a_source  ({ bios_s_a_source   , memory_s_a_source  , output_s_a_source    }),
+    .s_a_mask    ({ bios_s_a_mask     , memory_s_a_mask    , output_s_a_mask      }),
+    .s_a_address ({ bios_s_a_address  , memory_s_a_address , output_s_a_address   }),
+    .s_a_data    ({ bios_s_a_data     , memory_s_a_data    , output_s_a_data      }),
 
     // ======================
     // D Channel - Slaves
     // ======================
-    .s_d_valid   ({ bios_s_d_valid    , memory_s_d_valid     }),
-    .s_d_ready   ({ bios_s_d_ready    , memory_s_d_ready     }),
-    .s_d_opcode  ({ bios_s_d_opcode   , memory_s_d_opcode    }),
-    .s_d_param   ({ bios_s_d_param    , memory_s_d_param     }),
-    .s_d_size    ({ bios_s_d_size     , memory_s_d_size      }),
-    .s_d_source  ({ bios_s_d_source   , memory_s_d_source    }),
-    .s_d_data    ({ bios_s_d_data     , memory_s_d_data      }),
-    .s_d_corrupt ({ bios_s_d_corrupt  , memory_s_d_corrupt   }),
-    .s_d_denied  ({ bios_s_d_denied   , memory_s_d_denied    }),
+    .s_d_valid   ({ bios_s_d_valid    , memory_s_d_valid   , output_s_d_valid     }),
+    .s_d_ready   ({ bios_s_d_ready    , memory_s_d_ready   , output_s_d_ready     }),
+    .s_d_opcode  ({ bios_s_d_opcode   , memory_s_d_opcode  , output_s_d_opcode    }),
+    .s_d_param   ({ bios_s_d_param    , memory_s_d_param   , output_s_d_param     }),
+    .s_d_size    ({ bios_s_d_size     , memory_s_d_size    , output_s_d_size      }),
+    .s_d_source  ({ bios_s_d_source   , memory_s_d_source  , output_s_d_source    }),
+    .s_d_data    ({ bios_s_d_data     , memory_s_d_data    , output_s_d_data      }),
+    .s_d_corrupt ({ bios_s_d_corrupt  , memory_s_d_corrupt , output_s_d_corrupt   }),
+    .s_d_denied  ({ bios_s_d_denied   , memory_s_d_denied  , output_s_d_denied    }),
 
     // ======================
     // Base Addresses for Slaves
     // ======================
-    .base_addr   ({ bios_base_address , memory_base_address  }),
-    .addr_mask   ({ bios_size         , memory_size          })
+    .base_addr   ({ bios_base_address , memory_base_address , output_base_address  }),
+    .addr_mask   ({ bios_size         , memory_size         , output_size          })
 );
 
 // ──────────────────────────
@@ -359,6 +391,42 @@ tl_ul_bios #(
     .tl_d_data      (bios_s_d_data),
     .tl_d_corrupt   (bios_s_d_corrupt),
     .tl_d_denied    (bios_s_d_denied)
+);
+
+// ──────────────────────────
+// Instantiate output
+// ──────────────────────────
+tl_ul_output #(
+    .XLEN           (XLEN),
+    .SID_WIDTH      (SID_WIDTH),
+    .OUTPUTS        (6)
+) output_inst (
+    .clk            (clk),
+    .reset          (reset),
+
+    .outputs        (),
+
+    // TileLink A Channel
+    .tl_a_valid     (output_s_a_valid),
+    .tl_a_ready     (output_s_a_ready),
+    .tl_a_opcode    (output_s_a_opcode),
+    .tl_a_param     (output_s_a_param),
+    .tl_a_size      (output_s_a_size),
+    .tl_a_source    (output_s_a_source),
+    .tl_a_address   (output_s_a_address),
+    .tl_a_mask      (output_s_a_mask),
+    .tl_a_data      (output_s_a_data),
+
+    // TileLink D Channel
+    .tl_d_valid     (output_s_d_valid),
+    .tl_d_ready     (output_s_d_ready),
+    .tl_d_opcode    (output_s_d_opcode),
+    .tl_d_param     (output_s_d_param),
+    .tl_d_size      (output_s_d_size),
+    .tl_d_source    (output_s_d_source),
+    .tl_d_data      (output_s_d_data),
+    .tl_d_corrupt   (output_s_d_corrupt),
+    .tl_d_denied    (output_s_d_denied)
 );
 
 endmodule
